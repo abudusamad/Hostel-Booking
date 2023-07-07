f7e8c59ee0523a04339069774250d7857808885e
Abudu Samadu <abudusamed@gmail.com>
2023-07-07 16:02:35 +0000

    working with the api for the web

---
+++ src/components/api/index.js
@@ -0,0 +1,21 @@
+import cors from "cors";
+import { hostels } from "../../data/dummy";
+import express from "express";
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+	const { q } = req.query;
+
+	const keys = ["name", "namLoc", "address"];
+
+	const search = (data) => {
+		return data.filter((item) =>
+			keys.some((key) => item[key].toLowerCase().includes(q))
+		);
+	};
+
+	q ? res.json(search(hostels).slice(0, 10)) : res.json(hostels.slice(0, 10));
+});
+
+app.listen(5000, () => console.log("API is working!"));
