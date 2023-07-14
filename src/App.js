7b9ff3334b0d8200b67dccabacf146d0bcab0fe5
Abudu Samadu <abudusamed@gmail.com>
2023-07-14 15:20:38 +0000

    making the App responsive to the card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Route, Routes } from "react-router-dom";
-import HotelInfo from "./pages/register/HotelInfo";
+import { LoadingSkeleton } from "./components/LoadingSkeleton";
+import Login from "./pages/Home/Login";
+import Signup from "./pages/Home/Signup";
 import MyProfile from "./pages/register/MyProfile.jsx";
 import ComponentList from "./scenes/bar";
 import Calendar from "./scenes/calendar/calendar";
@@ -13,43 +15,46 @@ import Sidebar from "./scenes/global/Sidebar";
 import Topbar from "./scenes/global/Topbar";
 import Invoices from "./scenes/invoices";
 import { ColorModeContext, useMode } from "./theme";
-import Login from "./pages/Home/Login";
-import Signup from "./pages/Home/Signup";
-
+import { ImageGallery } from "./pages/ImageGallery";
 
 function App() {
 	const [theme, colorMode] = useMode();
 	const [isSidebar, setIsSidebar] = useState(true);
 	const [setAuth] = useState(false);
 
-
 	const queryClient = new QueryClient();
-
 
 	return (
 		<ColorModeContext.Provider value={colorMode}>
 			<ThemeProvider theme={theme}>
 				<CssBaseline />
-				<div className="app">
-					<Sidebar isSidebar={isSidebar} />
-					<main className="content">
-						<Topbar setIsSidebar={setIsSidebar} />
-						<QueryClientProvider client={queryClient}>
-							<Routes>
-								<Route path="/" element={<Login setAuth={setAuth} />} />
-								<Route path="/signup" element={<Signup setAuth={setAuth} />} />
-								<Route path="/dashboard" element={<Dashboard />} />
-								<Route path="/geography" element={<Geography />} />
-								<Route path="/calendar" element={<Calendar />} />
-								<Route path="/faq" element={<FAQ />} />
-								<Route path="/invoices" element={<Invoices />} />
-								<Route path="/bar" element={<ComponentList />} />
-								<Route path="/my-profile" element={<MyProfile />} />
-								<Route path="/reserve" element={<HotelInfo />} />
-							</Routes>
-						</QueryClientProvider>
-					</main>
-				</div>
+				<QueryClientProvider client={queryClient}>
+					<Suspense fallback={<LoadingSkeleton />}>
+						<div className="app">
+							<Sidebar isSidebar={isSidebar} />
+							<main className="content">
+								<Topbar setIsSidebar={setIsSidebar} />
+								<Routes>
+									<Route path="/" element={<Login setAuth={setAuth} />} />
+									<Route
+										path="/signup"
+										element={<Signup setAuth={setAuth} />}
+									/>
+									<Route path="/dashboard" element={<Dashboard />} />
+
+									<Route path="/geography" element={<Geography />} />
+									<Route path="/calendar" element={<Calendar />} />
+									<Route path="/faq" element={<FAQ />} />
+									<Route path="/invoices" element={<Invoices />} />
+									<Route path="/bar" element={<ComponentList />} />
+									<Route path="/my-profile" element={<MyProfile />} />
+
+									<Route path="/hostels" element={<ImageGallery />} />
+								</Routes>
+							</main>
+						</div>
+					</Suspense>
+				</QueryClientProvider>
 			</ThemeProvider>
 		</ColorModeContext.Provider>
 	);
